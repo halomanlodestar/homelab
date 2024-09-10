50b5e8348dd7fbd9138ab856b3570e5805e2d17d
Kunal Rana <kunalranarj2005@gmail.com>
2024-09-10 16:01:22 +0530

    Add status code with error message

--- src/api/controllers.rs
+++ src/api/controllers.rs
@@ -1,19 +1,26 @@
 use std::path::Path;
-use axum::{extract::Query, http::StatusCode, Json};
+use axum::{extract::Query, http::StatusCode, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
 use crate::files::{get_files, FileData};
 
 #[derive(Deserialize)]
-pub struct FilePath {
+pub struct Request {
   path: String
 }
 
+
 #[derive(Serialize)]
-pub struct Response {
+pub struct SuccessResponse {
   files: Vec<FileData>
 }
 
-pub async fn get_files_from(file_path: Query<FilePath>) -> Result<Json<Response>, StatusCode> {
+#[derive(Serialize)]
+pub struct ApiError {
+  message: String,
+  status: u16
+}
+
+pub async fn get_files_from(file_path: Query<Request>) -> Result<impl IntoResponse, impl IntoResponse> {
 
   let path = &file_path.path;
   let final_path = String::from("src/root_files_folder/") + path;
@@ -21,9 +28,11 @@ pub async fn get_files_from(file_path: Query<FilePath>) -> Result<Json<Response>
   let list: Vec<FileData> = match get_files(Path::new(&final_path)) {
     Ok(val) => val,
     Err(_) => {
-      return Err(StatusCode::NOT_FOUND);
+      // return Err(ApiError {status: StatusCode::NOT_FOUND.as_u16(), message: String::from("value")});
+      // return Err(StatusCode::NOT_FOUND);
+      return Err((StatusCode::NOT_FOUND,  String::from("No such directory found")));
     },
   };
 
-  return Ok(Json(Response { files: list }));
+  return Ok(Json(SuccessResponse { files: list }));
 }
