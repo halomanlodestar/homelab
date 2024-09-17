7497e0cae6157c8f9e27bfd29fdf70a4a9e1830c
Kunal Rana <kunalranarj2005@gmail.com>
2024-09-17 22:17:13 +0530

    Refactor and improved error handling

--- src/api/controllers/read_file.rs
+++ src/api/controllers/read_file.rs
@@ -1,9 +1,8 @@
-use std::path::PathBuf;
-
-use axum::{body::Body, extract::Query, http::HeaderMap, response::IntoResponse};
-use serde::Deserialize;
-
 use crate::files::read_file;
+use axum::{body::Body, extract::Query, http::HeaderMap, response::IntoResponse};
+use hyper::StatusCode;
+use serde::Deserialize;
+use std::path::PathBuf;
 
 #[derive(Deserialize)]
 pub struct Request {
@@ -17,18 +16,16 @@ pub async fn read_file_controller(
 ) -> Result<impl IntoResponse, impl IntoResponse> {
     let mut header = HeaderMap::new();
 
-    let file = match read_file(PathBuf::from(
+    let path = PathBuf::from(
         String::from("src/root_files_folder/") + &request.path.clone().unwrap_or(String::from("/")),
-    ))
-    .await
-    {
-        Ok(x) => x,
-        Err(_) => return Err(""),
-    };
+    );
 
-    // println!("{:?}", f);
+    let file = match read_file(path).await {
+        Ok(x) => x,
+        Err(err) => return Err((StatusCode::NOT_FOUND, err)),
+    };
 
     header.insert("Content-Type", "video/mp4".parse().unwrap());
 
-    return Ok((header, Body::from_stream(file)));
+    return Ok((StatusCode::OK, header, Body::from_stream(file)));
 }
